4bb10219cd2ff12da26266645ead0a3f144513f2
jusgolds <jusgolds@gmail.com>
2020-03-16 20:49:00 -0500

    Add category, categorytype and clue

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CategoryType, Category, Question
+
+myModels = [CategoryType, Category, Question]
+
+admin.site.register(myModels)
--- game/models.py
+++ game/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class CategoryType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    category_type = models.ForeignKey(CategoryType, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.category_name
+
+class Question(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    clue = models.CharField(max_length=200)
+    correct_ans = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.clue
